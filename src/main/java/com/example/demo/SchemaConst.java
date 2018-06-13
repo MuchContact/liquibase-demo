9ec29905a81e56f3b74206636b5bedda5f0c3cea
MuchContact <liuhengjiangis@foxmail.com>
2018-06-13 14:06:00 +0800

    调整changelog写法，因为Oracle11g不支持自增主键，需要显式创建序列

---
+++ src/main/java/com/example/demo/SchemaConst.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+/**
+ * 表集合常量
+ * @author yindl
+ *
+ */
+public class SchemaConst{
+	/** Oracle **/
+	public static final String DLSYS = "dlsys";
+	public static final String DLMIS = "dlmis";
+	public static final String DLGIS = "dlgis";
+	public static final String SDE = "sde";
+	public static final String MMS = "mms";
+	public static final String DLINIT = "dlinit";
+	public static final String UMSTAT = "umstat";
+	public static final String DLHIST = "dlhist";
+	public static final String DLTRANS = "dltrans";
+	public static final String DLDATA = "dldata";
+	public static final String DLZX = "dlzx";
+	
+	public static final String DLSYS_ = "dlsys.";
+	public static final String DLMIS_ = "dlmis.";
+	public static final String DLGIS_ = "dlgis.";
+	public static final String SDE_ = "sde.";
+	public static final String MMS_ = "mms.";
+	public static final String DLINIT_ = "dlinit.";
+	public static final String UMSTAT_ = "umstat.";
+	public static final String DLHIST_ = "dlhist.";
+	public static final String DLTRANS_ = "dltrans.";
+	public static final String DLDATA_ = "dldata.";
+	public static final String DLZX_ = "dlzx.";
+	
+
+}
