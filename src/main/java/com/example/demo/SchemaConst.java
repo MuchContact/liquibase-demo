477a0ef1ced4523ea9fbfa434f1fa052a8a3a808
MuchContact <liuhengjiangis@foxmail.com>
2018-06-13 14:08:10 +0800

    调整schemaconst

--- src/main/java/com/example/demo/SchemaConst.java
+++ src/main/java/com/example/demo/SchemaConst.java
@@ -6,30 +6,30 @@ package com.example.demo;
  *
  */
 public class SchemaConst{
-	/** Oracle **/
-	public static final String DLSYS = "dlsys";
-	public static final String DLMIS = "dlmis";
-	public static final String DLGIS = "dlgis";
-	public static final String SDE = "sde";
-	public static final String MMS = "mms";
-	public static final String DLINIT = "dlinit";
-	public static final String UMSTAT = "umstat";
-	public static final String DLHIST = "dlhist";
-	public static final String DLTRANS = "dltrans";
-	public static final String DLDATA = "dldata";
-	public static final String DLZX = "dlzx";
-	
-	public static final String DLSYS_ = "dlsys.";
-	public static final String DLMIS_ = "dlmis.";
-	public static final String DLGIS_ = "dlgis.";
-	public static final String SDE_ = "sde.";
-	public static final String MMS_ = "mms.";
-	public static final String DLINIT_ = "dlinit.";
-	public static final String UMSTAT_ = "umstat.";
-	public static final String DLHIST_ = "dlhist.";
-	public static final String DLTRANS_ = "dltrans.";
-	public static final String DLDATA_ = "dldata.";
-	public static final String DLZX_ = "dlzx.";
-	
+
+    /** MySQL 中都为空字符串  **/
+    public static final String DLSYS = "";
+    public static final String DLMIS = "";
+    public static final String DLGIS = "";
+    public static final String SDE = "";
+    public static final String MMS = "";
+    public static final String DLINIT = "";
+    public static final String UMSTAT = "";
+    public static final String DLHIST = "";
+    public static final String DLTRANS = "";
+    public static final String DLDATA = "";
+    public static final String DLZX = "";
+
+    public static final String DLSYS_ = "";
+    public static final String DLMIS_ = "";
+    public static final String DLGIS_ = "";
+    public static final String SDE_ = "";
+    public static final String MMS_ = "";
+    public static final String DLINIT_ = "";
+    public static final String UMSTAT_ = "";
+    public static final String DLHIST_ = "";
+    public static final String DLTRANS_ = "";
+    public static final String DLDATA_ = "";
+    public static final String DLZX_ = "";
 
 }
